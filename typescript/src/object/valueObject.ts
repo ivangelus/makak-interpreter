7aba49e6fc0fbf11f7af8a72ca868e779d4b82eb
Ivan Gelo <geloivan21@gmail.com>
2024-11-03 11:35:11 +0100

    object value, int, bool, null

---
+++ typescript/src/object/valueObject.ts
@@ -0,0 +1,51 @@
+
+type ValueObjectType = string;
+
+const INTEGER_OBJECT = 'INTEGER';
+const BOOLEAN_OBJECT = 'BOOLEAN';
+const NULL_OBJECT = 'NULL';
+
+export class ValueObject {
+    public getType(): ValueObjectType {
+        return '';
+    }
+
+    public inspect(): string {
+        return '';
+    }
+}
+
+export class MonkeyInteger extends ValueObject {
+    value: number;
+
+    public inspect(): string {
+        return this.value.toString();
+    }
+
+    public getType(): ValueObjectType {
+        return INTEGER_OBJECT;
+    }
+}
+
+export class MonkeyBoolean extends ValueObject {
+    value: boolean;
+
+    public inspect(): string {
+        return this.value.toString();
+    }
+
+    public getType(): ValueObjectType {
+        return BOOLEAN_OBJECT;
+    }
+}
+
+export class MonkeyNull extends ValueObject {
+
+    public inspect(): string {
+        return 'null';
+    }
+
+    public getType(): ValueObjectType {
+        return NULL_OBJECT;
+    }
+}
